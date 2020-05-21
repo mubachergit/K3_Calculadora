4732c52a9caa816ff1276f80393a775e048919e2
mubacher <mubacheralrifai@gmail.com>
2020-05-21 15:30:32 +0200

    Tests con listas de botones romanos y normales

--- test_keyboard.py
+++ test_keyboard.py
@@ -21,6 +21,8 @@ class TestKeyboard(tkinterTestCase.TkTestCase):
         for btn in self.k.children.values():
             self.assertIsInstance(btn, calculator.CalcButton)
         self.assertEqual(len(self.k.children), 18)
+        self.assertEqual(len(self.k.listaBRomanos), 0)
+        self.assertEqual(len(self.k.listaBNormales), 18)
 
     def test_render_roman_Ok(self):
         teclado_romano = calculator.Keyboard(self.root, "R")
@@ -32,11 +34,22 @@ class TestKeyboard(tkinterTestCase.TkTestCase):
         for btn in teclado_romano.children.values():
             self.assertIsInstance(btn, calculator.CalcButton)
         self.assertEqual(len(teclado_romano.children), 13)
+        self.assertEqual(len(teclado_romano.listaBRomanos), 13)
+
 
         teclado_romano.update()
         teclado_romano.destroy()
 
-
+    def test_change_status_keyboard(self):
+        self.assertEqual(self.k.status, "N")
+        self.k.status = "R"
+        for btn in self.k.children.values():
+            self.assertIsInstance(btn, calculator.CalcButton)
+        self.assertEqual(len(self.k.listaBRomanos), 13)
+        self.assertEqual(self.k.status, "R")
+        self.assertEqual(len(self.k.listaBRomanos), 13)
+        self.assertEqual(len(self.k.listaBNormales), 18)
+        self.assertEqual(len(self.k.children), 31)
 
 
 if __name__ == "__main__":
